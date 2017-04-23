62f8f42c8fd9207495bc0c64d89e4f871447d914
Benjamin Reed <benvreed@gmail.com>
2017-04-23 01:33:20 -0500

    Add images and implement pysolr search

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, Response
+import pysolr
+import json
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,6 +11,25 @@ def index():
 @app.route("/about")
 def about():
     return render_template('about.html', addressee='Ben!')
+
+@app.route("/search")
+def search():
+    query = request.args.get('query')
+    solr = pysolr.Solr('http://localhost:8983/solr/chewigy/', timeout=60)
+    results = solr.search(query)
+
+    results_json = list()
+
+    for result in results.docs:
+        results_json.append(result)
+
+    response = Response(json.dumps({
+        'results': results_json,
+        'time': results.qtime
+    }))
+
+    response.headers['Content-type'] = 'application/json'
+    return response
 
 @app.route("/robots.txt")
 def robots():
